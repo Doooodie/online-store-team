07171b89016c17631180632f9fd60ecd651ad3b2
LineCoran <vppisya@gmail.com>
2022-12-17 21:13:32 +0300

    feat: select component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,27 +3,43 @@ import dataProducts from '../../assets/json/products.json';
 import ProductList from './components/ProductList/ProductList';
 import { useMemo, useState } from 'react';
 import './Home.css';
-import { MyInput } from './UI/MyInput';
+import MyInput from './UI/input/MyInput';
+import MySelect from './UI/select/MySelect';
 
 function Home() {
   const productsArray = dataProducts.products;
   const [products, setProducts] = useState(productsArray);
   const [searchQuery, setSearchQuery] = useState('');
 
+  const [sortQuery, setSortQuery] = useState('default');
 
-  
+
+
   const searchProducts = useMemo(() => {
-    console.log(searchQuery)
     return [...products].filter((product) => product.title.toLowerCase().includes(searchQuery.toLowerCase()))
   }, [searchQuery])
 
   return (
     <div>
+      <div>
         <MyInput 
-          placeholder='Search...'
-          value={searchQuery}
-          onChange={(e: any) => setSearchQuery(e.target.value)}
-        />
+            placeholder='Search...'
+            value={searchQuery}
+            onChange={(e: any) => setSearchQuery(e.target.value)}
+          />
+
+          <MySelect 
+            defaultValue='Sort options...'
+            options={[
+              {value: 'priceASC', name: 'Sort by price ASC'},
+              {value: 'priceDESC', name: 'Sort by price DESC'},
+              {value: 'ratingASC', name: 'Sort by rating ASC'},
+              {value: 'ratingDESC', name: 'Sort by rating DESC'},
+              {value: 'discountASC', name: 'Sort by discount ASC'},
+              {value: 'discountDESC', name: 'Sort by discount DESC'},
+            ]}
+          />
+      </div>
         <ProductList products={searchProducts}/>
     </div>
     
