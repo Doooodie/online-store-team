5f3859fcb89bafec054508b3ebc06771657f2730
LineCoran <vppisya@gmail.com>
2022-12-18 16:10:37 +0300

    feat: search search by any data

--- src/pages/Home/hooks/useFilter.tsx
+++ src/pages/Home/hooks/useFilter.tsx
@@ -12,15 +12,28 @@ export function useSortedPost(products: IProduct[], sort: string) {
   return getSortedProducts;
 }
 
-export function useFilterProducts(products: IProduct[], sort: string, query: string) {
-  const sortedProducts = useSortedPost(products, sort);
-  const getSortedAndSearchProducts = useMemo(() => {
+export function useSearchProducts(products: IProduct[], query: string) {
+  const searchProducts = useMemo(() => {
     if (query) {
-      return [...sortedProducts].filter((product) =>
-        product.title.toLowerCase().includes(query.toLowerCase()),
+      return [...products].filter(
+        (product) =>
+          product.title.toLowerCase().includes(query.toLowerCase()) ||
+          product.description.toLowerCase().includes(query.toLowerCase()) ||
+          product.brand.toLowerCase().includes(query.toLowerCase()) ||
+          product.category.toLowerCase().includes(query.toLowerCase()) ||
+          String(product.price).includes(query.toLowerCase()) ||
+          String(product.discountPercentage).includes(query.toLowerCase()) ||
+          String(product.rating).includes(query.toLowerCase()) ||
+          String(product.stock).includes(query.toLowerCase()),
       );
     }
-    return sortedProducts;
-  }, [sortedProducts, query]);
-  return getSortedAndSearchProducts;
+    return [...products];
+  }, [products, query]);
+  return searchProducts;
+}
+
+export function useFilterProducts(products: IProduct[], sort: string, query: string) {
+  const sortedProducts = useSortedPost(products, sort);
+  const sortedAndSearchProducts = useSearchProducts(sortedProducts, query);
+  return sortedAndSearchProducts;
 }
