aabbdff27570a344ce352fe0239fe1859e06b34d
LineCoran <vppisya@gmail.com>
2022-12-18 15:58:48 +0300

    feat: add Post not Find is products length = 0

--- src/pages/Home/components/ProductList/ProductList.tsx
+++ src/pages/Home/components/ProductList/ProductList.tsx
@@ -3,14 +3,20 @@ import Product from '../Product/Product';
 import { IProduct } from '../../types/types';
 
 export interface ProductListProps {
-  products: IProduct[];
+  products: IProduct[] | undefined;
 }
 export default function ProductList({ products }: ProductListProps) {
-  return (
-    <div className={styles.productList}>
-      {products.map((item) => (
-        <Product product={item} key={item.id} />
-      ))}
-    </div>
-  );
+  if (products?.length === 0) {
+    return <h1>Posts not find</h1>;
+  }
+  if (products !== undefined) {
+    return (
+      <div className={styles.productList}>
+        {products.map((item) => (
+          <Product product={item} key={item.id} />
+        ))}
+      </div>
+    );
+  }
+  return <h1>Posts not find</h1>;
 }
