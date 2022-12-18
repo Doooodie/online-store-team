49cd24e91836676771dec5d8454007563c5a7d22
LineCoran <vppisya@gmail.com>
2022-12-18 14:26:53 +0300

    feat: create productFilter component with sort and search

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,48 +1,23 @@
-/* eslint-disable */
-import dataProducts from '../../assets/json/products.json';
+import { useState } from 'react';
 import ProductList from './components/ProductList/ProductList';
-import { useMemo, useState } from 'react';
+import Container from './components/Container/Container';
+import ProductFilter from './components/ProductsFilter/ProductFilter';
+import { IFilter } from './types/types';
+import { useFilterProducts } from './hooks/useFilter';
+import dataProducts from '../../assets/json/products.json';
 import './Home.css';
-import MyInput from './UI/input/MyInput';
-import MySelect from './UI/select/MySelect';
 
 function Home() {
   const productsArray = dataProducts.products;
   const [products, setProducts] = useState(productsArray);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const [sortQuery, setSortQuery] = useState('default');
-
-
-
-  const searchProducts = useMemo(() => {
-    return [...products].filter((product) => product.title.toLowerCase().includes(searchQuery.toLowerCase()))
-  }, [searchQuery])
+  const [filter, setFilter] = useState<IFilter>({ sort: 'default', query: '' });
+  const filteredProducts = useFilterProducts(products, filter.sort, filter.query);
 
   return (
-    <div>
-      <div>
-        <MyInput 
-            placeholder='Search...'
-            value={searchQuery}
-            onChange={(e: any) => setSearchQuery(e.target.value)}
-          />
-
-          <MySelect 
-            defaultValue='Sort options...'
-            options={[
-              {value: 'priceASC', name: 'Sort by price ASC'},
-              {value: 'priceDESC', name: 'Sort by price DESC'},
-              {value: 'ratingASC', name: 'Sort by rating ASC'},
-              {value: 'ratingDESC', name: 'Sort by rating DESC'},
-              {value: 'discountASC', name: 'Sort by discount ASC'},
-              {value: 'discountDESC', name: 'Sort by discount DESC'},
-            ]}
-          />
-      </div>
-        <ProductList products={searchProducts}/>
-    </div>
-    
+    <Container>
+      <ProductFilter filter={filter} setFilter={setFilter} />
+      <ProductList products={filteredProducts} />
+    </Container>
   );
 }
 
