bbfce8c1c25a8329a8fcf5aaf165737162704619
Doooodie <unfair.slenderman@gmail.com>
2022-12-28 14:27:39 +0300

    feat: add styles for 404 page

--- src/components/pages/NoPage/NoPage.tsx
+++ src/components/pages/NoPage/NoPage.tsx
@@ -1,7 +1,11 @@
 import './NoPage.css';
 
 function NoPage() {
-  return <h1 className='404'>404</h1>;
+  return (
+    <main className='main container'>
+      <h1 className='no-page-header'>The page you are looking for does not exist 😔</h1>
+    </main>
+  );
 }
 
 export default NoPage;
