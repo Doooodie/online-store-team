cbbe096e8efcd3f3159b68e0f63c5f08569b396a
LineCoran <vppisya@gmail.com>
2023-01-09 23:49:36 +0300

    feat: modal

--- src/components/pages/Cart/components/Summary/Summary.tsx
+++ src/components/pages/Cart/components/Summary/Summary.tsx
@@ -1,6 +1,13 @@
+import * as React from 'react';
+import Modal from '@mui/material/Modal';
 import './Summary.css';
+import MyForm from '../Form/MyForm';
 
 function Summary() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <section className='summary-container'>
       <h2 className='summary-heading'>Summary</h2>
@@ -14,9 +21,12 @@ function Summary() {
       <p className='summary-promo-info'>
         Promo for test: <strong>RS</strong>
       </p>
-      <button type='button' className='buy-button summary-button'>
+      <button onClick={handleOpen} type='button' className='buy-button summary-button'>
         Buy Now
       </button>
+      <Modal open={open} onClose={handleClose}>
+        <MyForm />
+      </Modal>
     </section>
   );
 }
