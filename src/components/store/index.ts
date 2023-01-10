9c7b46d2e10d9a4f2c1b69031567ecec52eeb6ae
Doooodie <unfair.slenderman@gmail.com>
2023-01-10 20:17:23 +0100

    feat: add working "buy now" button

--- src/components/store/index.ts
+++ src/components/store/index.ts
@@ -13,16 +13,19 @@ import storage from 'redux-persist/lib/storage';
 import cartReducer from './cartSlice';
 import filterReducer from './filterSlice';
 import promoReducer from './promoSlice';
+import modalReducer from './modalSlice';
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   filter: filterReducer,
   promo: promoReducer,
+  modal: modalReducer,
 });
 
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['modal'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
---
+++ src/components/store/modalSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type ModalState = {
+  isOpened: boolean;
+};
+
+const initialState: ModalState = {
+  isOpened: false,
+};
+
+const ModalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    changeModalVisibility(state, action: PayloadAction<ModalState>) {
+      const currentState = state;
+      currentState.isOpened = action.payload.isOpened;
+    },
+  },
+});
+
+export default ModalSlice.reducer;
+export const { changeModalVisibility } = ModalSlice.actions;
+export type { ModalState };
